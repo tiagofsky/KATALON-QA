a8ce2c369be995006794d38b4e1a8ea04daeef4a
tiagofsky <144699755+tiagofsky@users.noreply.github.com>
2023-12-11 16:36:38 +0100

    SIGN OUT

---
+++ SignOUT/Test Suites/Profiles.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Profiles</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a790a07a-6483-42e1-84e8-5154d51833e9</testSuiteGuid>
+   <testCaseLink>
+      <guid>20642f1f-ed39-4c0d-8687-17b908d06f10</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Profiles/Sign out</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
